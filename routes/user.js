08f037c7f38a6167a44734f1e2c14cd7f48562e0
ccaporale1 <ccapmag@gmail.com>
2022-04-13 20:34:49 -0400

    initial user route changes

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,20 @@ const express = require('express');
 
 const usersRouter = express.Router();
 
+const validateUser = (user) => {
+  //username
+  if (typeof user.username !== 'string') {
+    //error
+  }
+  //password
+  if (typeof user.password !== 'string') {
+    //error
+  }
+  //email address
+  if (typeof user.email !== 'string' ) {
+    //error
+  }
+}
 //GET USER
 app.get('/:id', (req, res, next) => {
   db.query('SELECT * FROM users WHERE id = $1', [req.params.id], (err, result) => {
