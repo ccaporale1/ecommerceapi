97dd0300f043738f70db6f7ccabafd290792cc22
ccaporale1 <ccapmag@gmail.com>
2022-04-19 23:34:12 -0400

    product router framework

--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,26 @@ const express = require('express');
 
 const productsRouter = express.Router();
 
-//GET PRODUCT INFO
+//GET ALL PRODUCTS INFO
+productsRouter.get('/', async (req,res,next) => {
 
+});
+
+//GET PRODUCT BY ID
+productsRouter.get('/:id', async (req,res,next) => {
+
+});
 //POST NEW PRODUCT
+productsRouter.post('/:id', async (req,res,next) => {
+
+});
 
 //PUT UPDATE TO PRODUCT
+productsRouter.put('/:id', async (req,res,next) => {
+
+});
 
 //DELETE PRODUCT FROM INVENTORY
+productsRouter.delete('/:id', (req,res,next) => {
+
+});
